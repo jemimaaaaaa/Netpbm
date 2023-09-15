a5f7bf4e71c74b2b65713dcd5646071825588ded
kjemima <jemima.kisolokelo@ynov.com>
2023-09-15 11:55:07 +0200

    dd

--- exam 2/PGCD.go
+++ exam 2/PGCD.go
@@ -1,4 +1,15 @@
-package piscine
+package exam
 
 func PGCD(p int, q int) int {
+	ab := p / q
+	if p < 0 {
+		return 0
+	if q < 0 {
+		return 0
+	} else if {
+		for i := 1, i < ab; i++
+		}
+	}
+	return ab
+}
 	
